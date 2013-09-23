898826f150cac2044e113ced86b4c9175ae966e9
Brad Thompson <thompsonb@wl.k12.in.us>
2013-09-23 15:40:03 -0400

    ui tweaks & more filtering

--- app/controllers/welcome_controller.rb
+++ app/controllers/welcome_controller.rb
@@ -5,7 +5,9 @@ class WelcomeController < ApplicationController
           @top_services = Service.where(:server_id => nil)
       else
           @services = Service.where(:technical => false)
-          @top_services = @services.where(:server_id => Service.where(:technical => true).ids)
+          @top_services = @services.where(:server_id => Service.where(:technical => true).ids + [nil])
       end
+
+      @external = request.remote_ip.match(/^10\.10\./) ? true : false
   end
 end
